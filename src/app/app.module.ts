93428b4be51e5795731ff4f2a0593f47404f9f81
Abdul Basit <brcoopbasit@gmail.com>
2019-03-24 21:59:51 -0400

    added primitive dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { ResumeComponent } from './resume/resume.component';
-import { PdfViewerModule } from 'ng2-pdf-viewer'
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { DropdownDirective } from './shared/dropdown.directive'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    ResumeComponent
+    ResumeComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
